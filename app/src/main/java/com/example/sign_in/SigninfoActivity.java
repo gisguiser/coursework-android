1e780ef3c20c2e327e11a6addc4d4ad6d1cccf3a
陈明洋 <2687828821@qq.com>
2020-06-30 18:47:19 +0800

    修改为解析Gson

--- app/src/main/java/com/example/sign_in/SigninfoActivity.java
+++ app/src/main/java/com/example/sign_in/SigninfoActivity.java
@@ -6,10 +6,13 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import com.example.util.HttpRequest;
+import com.google.gson.Gson;
 
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.List;
 
 /**代码中TextView参数的说明：   sumnum:签到总人数  desc:签到描述   key:签到码     endtime:截止时间
                               statue:签到码状态  person:签到人员姓名
@@ -34,6 +37,7 @@ public class SigninfoActivity extends Activity {
         //获取前面页面传送的dign_id
         Bundle bunde=this.getIntent().getExtras();
         sign_id=bunde.getString("text");
+
         //发送http请求
         SharedPreferences config = getSharedPreferences("config", MODE_PRIVATE);
         String token = config.getString("token", "");
@@ -43,31 +47,63 @@ public class SigninfoActivity extends Activity {
         }
 
     }
-    protected void GSONAnalysis(String string) {
-        JSONObject object = null;
-        try {
-            object = new JSONObject(string);
-        } catch (JSONException e) {
-            e.printStackTrace();
+    protected void GSONAnalysis(String result) {
+        Gson gson=new Gson();
+        //解析http请求返回的gson
+        user_token userToken=gson.fromJson(result,user_token.class);
+        String state=userToken.getState();
+        if(state == "true") {
+            int count=userToken.getPayloads().getSign_count();
+            sumnum.setText(count);
+            String message=userToken.getPayloads().getSign_message();
+            desc.setText(message);
+            String code=userToken.getPayloads().getSign_code();
+            key.setText(code);
+            String limit=userToken.getPayloads().getTime_limit();
+            endtime.setText(limit);
+            List<String> person_name_list=userToken.getPayloads().sign_person;
+            for(String person_name:person_name_list){
+                person.setText(person_name);
+            }
+
         }
-        /**
-         * 在你获取的string这个JSON对象中，提取你所需要的信息。
-         */
-        JSONObject ObjectInfo = object.optJSONObject("payload");
-        String state = ObjectInfo.optString("state");
-        String  count = ObjectInfo.optString("sign_count");
-        String message = ObjectInfo.optString("sign_message");
-        String code = ObjectInfo.optString("sign_code");
-        String limit = ObjectInfo.optString("time_limit");
-        String person_name = ObjectInfo.optString("sign_person");
-        sumnum.setText(count);
-        desc.setText(message);
-        key.setText(code);
-        endtime.setText(limit);
-        statue.setText(state);
-        person.setText(person_name);
 
     }
+    //获取gsonz中state和payload
+    class user_token{
+        private String state;
+        private Payloads payloads;
+        public String getState(){
+            return state;
+        }
+        public Payloads getPayloads(){
+            return payloads;
+        }
+    }
+    class Payloads{
+        private int sign_count;
+        private String sign_message;
+        List<String> sign_person;
+        String sign_code;
+        String time_limit;
+        public int getSign_count(){
+            return sign_count;
+        }
+        public String getSign_message(){
+            return sign_message;
+        }
+        public List<String> getSign_person(){
+            return sign_person;
+        }
+        public String getSign_code(){
+            return sign_code;
+        }
+        public String getTime_limit(){
+            return time_limit;
+        }
+
+    }
+
 
 }
 
